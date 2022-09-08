6aa01bc08d3aa2a60d23fbd3d524328468a00491
Eloan Ferreira <98027423+emffor@users.noreply.github.com>
2022-09-08 16:06:54 -0300

    Configurando Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
+
+//resetCSS é um prop que redefine o CSS padrão do navegador
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
